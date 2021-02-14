b6442da1a30fcda0c5b964611e75357aa4c6d15f
KuroIShiro <pxpxpx2015@gmail.com>
2021-02-14 18:23:06 +0300

    Added just one comment line

--- src/scripts/worlds/Menu.gd
+++ src/scripts/worlds/Menu.gd
@@ -9,4 +9,5 @@ func _on_SettingsButton_pressed():
 	pass # Replace with function body.
 
 func _on_PlayButton_pressed():
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://src/scenes/worlds/TestWorld.tscn")
